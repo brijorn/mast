f1753e84260a2a5783fc9ca06e6f7f2b04d61606
Brijorn <brijorn@outlook.com>
2026-06-28 18:27:33 -0400

    Fix Mast CLI args on Android

--- cmd/mast/main.go
+++ cmd/mast/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
+	"path/filepath"
+	"runtime"
 
 	"github.com/alecthomas/kong"
 	mastcli "github.com/brijorn/mast/internal/cli"
@@ -18,10 +21,45 @@ var cli struct {
 
 func main() {
 
-	ctx := kong.Parse(&cli)
-	err := ctx.Run()
+	parser, err := kong.New(&cli)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, err := parser.Parse(commandArgs())
+	parser.FatalIfErrorf(err)
+	err = ctx.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+}
+
+func commandArgs() []string {
+	args := os.Args[1:]
+	if runtime.GOOS != "android" || len(args) == 0 {
+		return args
+	}
+
+	executable, err := os.Executable()
+	if err != nil {
+		return args
+	}
+	return trimAndroidExecutableArg(args, executable)
+}
+
+func samePath(a string, b string) bool {
+	if a == "" || b == "" {
+		return false
+	}
+	return filepath.Clean(a) == filepath.Clean(b)
+}
+
+func trimAndroidExecutableArg(args []string, executable string) []string {
+	for len(args) > 0 && samePath(args[0], executable) {
+		args = args[1:]
+	}
+	for len(args) > 0 && samePath(args[len(args)-1], executable) {
+		args = args[:len(args)-1]
+	}
+	return args
 }
